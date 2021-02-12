4d13cb5616dfa1ec2b972800cd5de905b654acf8
Stryk001 <101280385@georgebrown.ca>
2021-02-12 18:22:02 -0500

    grades demo lab

--- LabDemos/Source.cpp
+++ LabDemos/Source.cpp
@@ -1,21 +1,44 @@
-//calculate the area of a rectangle
 #include <iostream>;
-#include <cmath>;
 using namespace std;
 
 int main()
 {
-	const double PI = 3.13159;
-	double area, radius;
+	//create name constants to hold minimun score required for each letter grade
+	const int MIN_A_SCORE = 80,
+		MIN_B_SCORE = 70,
+		MIN_C_SCORE = 60,
+		MIN_D_SCORE = 50;
 
-	cout << "This programe will calculate the area of a circle.\n";
+	int testScore; //holds a numeric score
+	char grade;   //hold a letter grade
 
-	cout << "What is the radius of the circle?\n";
-	cin >> radius;
+	cout << "enter your numeric test score and i will tell uou the letter grade: ";
+	cin >> testScore;
 
-	area = PI * pow(radius, 2);
-	cout << "The area of the circle is " << area << endl;
+	//determin the letter grade
+	if (testScore >= MIN_A_SCORE)
+	{
+		grade = 'A';
+	}
+	else if (testScore >= MIN_B_SCORE)
+	{
+		grade = 'B';
+	}
+	else if (testScore >= MIN_C_SCORE)
+	{
+		grade = 'C';
+	}
+	else if (testScore >= MIN_D_SCORE)
+	{
+		grade = 'D';
+	}
+	else if (testScore >= 0)
+	{
+		grade = 'F';
+	}
+
+	cout << "your grade is: " << grade << endl;
+
 
 	return 0;
-
 }
