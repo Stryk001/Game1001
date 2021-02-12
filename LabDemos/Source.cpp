2136abdc68fd4e69c98016b30b0bc1f35638030f
Stryk001 <101280385@georgebrown.ca>
2021-02-12 15:23:21 -0500

    area of a rectangle

--- LabDemos/Source.cpp
+++ LabDemos/Source.cpp
@@ -1,8 +1,22 @@
-#include <iostream>
+//calculate the area of a rectangle
+#include <iostream>;
 using namespace std;
 
 int main()
 {
+	int  length, width, area;
+
+	cout << "The programe calculates the area of the rectangle.\n";
+
+	cout << "what is the length of the rectangle (as an integer)?\n";
+	cin >> length;
+	cout << "What is the width of the rectangle (as an integer)?\n";
+	cin >> width;
+
+	area = length * width;
+
+	cout << "The area of the rectangle is " << area;
 
 	return 0;
+
 }
