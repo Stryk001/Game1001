c6b1bc40b1aa105fa1e6536cc244a69d7ccbf7f0
Stryk001 <101280385@georgebrown.ca>
2021-02-12 19:09:57 -0500

    example of range and nested if statements

--- LabDemos/Source.cpp
+++ LabDemos/Source.cpp
@@ -4,41 +4,53 @@ using namespace std;
 int main()
 {
 	//create name constants to hold minimun score required for each letter grade
-	const int MIN_A_SCORE = 80,
-		MIN_B_SCORE = 70,
-		MIN_C_SCORE = 60,
-		MIN_D_SCORE = 50;
+	const int
+		A_SCORE = 80,
+		B_SCORE = 70,
+		C_SCORE = 60,
+		D_SCORE = 50,
+		MIN_SCORE = 0,
+		MAX_SCORE = 100;
 
 	int testScore; //holds a numeric score
 	char grade;   //hold a letter grade
+	bool possibleScore = true;
 
-	cout << "enter your numeric test score and i will tell uou the letter grade: ";
+	cout << "Enter your numeric test score and i will tell uou the letter grade: ";
 	cin >> testScore;
 
-	//determin the letter grade
-	if (testScore >= MIN_A_SCORE)
+	//check if input is valid
+	if (testScore >= MIN_SCORE && testScore <= MAX_SCORE)
 	{
-		grade = 'A';
-	}
-	else if (testScore >= MIN_B_SCORE)
-	{
-		grade = 'B';
-	}
-	else if (testScore >= MIN_C_SCORE)
-	{
-		grade = 'C';
-	}
-	else if (testScore >= MIN_D_SCORE)
-	{
-		grade = 'D';
-	}
-	else if (testScore >= 0)
-	{
-		grade = 'F';
-	}
+		//score is valid
+		if (testScore >= A_SCORE)
+		{
+			cout << "your grade is A.\n";
+		}
+		else if (testScore >= B_SCORE)
+		{
+			cout << "your grade is B.\n";
+		}
+		else if (testScore >= C_SCORE)
+		{
+			cout << "your grade is C.\n";
+		}
+		else if (testScore >= D_SCORE)
+		{
+			cout << "your grade is D.\n";
+		}
+		else
+		{
+			cout << "your grade is F.\n";
+		}
 
-	cout << "your grade is: " << grade << endl;
-
+	}
+	else
+	{
+		//the score is invalid
+		cout << "\nThat is an invalid score. Run  the program again and enter a value in the range of\n"
+			<< MIN_SCORE << "through " << MAX_SCORE << ".\n";
+	}
 
 	return 0;
 }
